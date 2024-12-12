d1a85367119d358128616f15486a293d0bf43b28
Yeqin Gong <gon@gyeq.in>
2024-12-12 16:18:45 +0800

    fixed bugs

--- src/generators/javascript/motion.js
+++ src/generators/javascript/motion.js
@@ -1,88 +1,88 @@
 import { javascriptGenerator } from './generator';
 
-javascriptGenerator['motion_attack'] = (block) => {
+javascriptGenerator['motion_attack'] = function (block) {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const directionValue = javascriptGenerator.valueToCode(block, 'DIRECTION', javascriptGenerator.ORDER_NONE) || 0;
-  const distanceValue = javascriptGenerator.valueToCode(block, 'DISTANCE', javascriptGenerator.ORDER_NONE) || 100;
-  code += `await target.util.attack(${directionValue}, ${distanceValue});\n${this.AWAIT_ABORT}`;
+  const directionValue = this.valueToCode(block, 'DIRECTION', this.ORDER_NONE) || 0;
+  const distanceValue = this.valueToCode(block, 'DISTANCE', this.ORDER_NONE) || 100;
+  code += this.wrapAsync(`await target.util.attack(${directionValue}, ${distanceValue});`);
   return code;
 };
 
 javascriptGenerator['motion_move'] = (block) => {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const directionValue = javascriptGenerator.valueToCode(block, 'DIRECTION', javascriptGenerator.ORDER_NONE) || 0;
-  const speedValue = javascriptGenerator.valueToCode(block, 'SPEED', javascriptGenerator.ORDER_NONE) || 100;
-  code += `await target.util.move(${directionValue}, ${speedValue});\n${this.AWAIT_ABORT}`;
+  const directionValue = this.valueToCode(block, 'DIRECTION', this.ORDER_NONE) || 0;
+  const speedValue = this.valueToCode(block, 'SPEED', this.ORDER_NONE) || 100;
+  code += this.wrapAsync(`await target.util.move(${directionValue}, ${speedValue});`);
   return code;
 };
 
 javascriptGenerator['motion_turnright'] = (block) => {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const degreesCode = javascriptGenerator.valueToCode(block, 'DEGREES', javascriptGenerator.ORDER_NONE) || 0;
-  code += `await target.util.turnRight(${degreesCode});\n${this.AWAIT_ABORT}`;
+  const degreesCode = this.valueToCode(block, 'DEGREES', this.ORDER_NONE) || 0;
+  code += this.wrapAsync(`await target.util.turnRight(${degreesCode});`);
   return code;
 };
 
 javascriptGenerator['motion_turnleft'] = (block) => {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const degreesCode = javascriptGenerator.valueToCode(block, 'DEGREES', javascriptGenerator.ORDER_NONE) || 0;
-  code += `await target.util.turnLeft(${degreesCode});\n${this.AWAIT_ABORT}`;
+  const degreesCode = this.valueToCode(block, 'DEGREES', this.ORDER_NONE) || 0;
+  code += this.wrapAsync(`await target.util.turnLeft(${degreesCode});`);
   return code;
 };
 
 javascriptGenerator['motion_pointindirection'] = (block) => {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const directionCode = javascriptGenerator.valueToCode(block, 'DIRECTION', javascriptGenerator.ORDER_NONE) || 0;
-  code += `await target.util.setDirection(+${directionCode});\n${this.AWAIT_ABORT}`;
+  const directionCode = this.valueToCode(block, 'DIRECTION', this.ORDER_NONE) || 0;
+  code += this.wrapAsync(`await target.util.setDirection(+${directionCode});`);
   return code;
 };
 
 javascriptGenerator['motion_setspeed'] = (block) => {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const speedCode = javascriptGenerator.valueToCode(block, 'SPEED', javascriptGenerator.ORDER_NONE) || 0;
+  const speedCode = this.valueToCode(block, 'SPEED', this.ORDER_NONE) || 0;
   code += `target.util.speed = ${speedCode};\n`;
   return code;
 };
 
 javascriptGenerator['motion_stop'] = (block) => {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
   code += `target.util.speed = 0;\n`;
   return code;
 };
 
 javascriptGenerator['motion_xposition'] = (block) => {
-  return ['target.util.x', javascriptGenerator.ORDER_ATOMIC];
+  return ['target.util.x', this.ORDER_ATOMIC];
 };
 
 javascriptGenerator['motion_yposition'] = (block) => {
-  return ['target.util.y', javascriptGenerator.ORDER_ATOMIC];
+  return ['target.util.y', this.ORDER_ATOMIC];
 };
 
 javascriptGenerator['motion_speed'] = (block) => {
-  return ['target.util.currentSpeed', javascriptGenerator.ORDER_ATOMIC];
+  return ['target.util.currentSpeed', this.ORDER_ATOMIC];
 };
 
 javascriptGenerator['motion_direction'] = (block) => {
-  return ['target.util.direction', javascriptGenerator.ORDER_ATOMIC];
+  return ['target.util.direction', this.ORDER_ATOMIC];
 };
--- src/generators/javascript/sensing.js
+++ src/generators/javascript/sensing.js
@@ -1,40 +1,40 @@
 import { javascriptGenerator } from './generator';
 
-javascriptGenerator['sensing_scanwidth'] = (block) => {
+javascriptGenerator['sensing_scanwidth'] = function (block) {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const widthValue = javascriptGenerator.valueToCode(block, 'WIDTH', javascriptGenerator.ORDER_NONE) || 5;
+  const widthValue = this.valueToCode(block, 'WIDTH', this.ORDER_NONE) || 5;
   code += `target.util.scanWidth = ${widthValue};\n`;
   return code;
 };
 
-javascriptGenerator['sensing_scan'] = (block) => {
+javascriptGenerator['sensing_scan'] = function (block) {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const directionValue = javascriptGenerator.valueToCode(block, 'DIRECTION', javascriptGenerator.ORDER_NONE) || 0;
+  const directionValue = this.valueToCode(block, 'DIRECTION', this.ORDER_NONE) || 0;
   code += `(await target.util.scan(${directionValue}) !== Infinity)`;
-  return [code, javascriptGenerator.ORDER_EQUALITY];
+  return [code, this.ORDER_EQUALITY];
 };
 
-javascriptGenerator['sensing_scandistance'] = (block) => {
+javascriptGenerator['sensing_scandistance'] = function (block) {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
-  const directionValue = javascriptGenerator.valueToCode(block, 'DIRECTION', javascriptGenerator.ORDER_NONE) || 0;
+  const directionValue = this.valueToCode(block, 'DIRECTION', this.ORDER_NONE) || 0;
   code += `(await target.util.scan(${directionValue}))`;
-  return [code, javascriptGenerator.ORDER_FUNCTION_CALL];
+  return [code, this.ORDER_FUNCTION_CALL];
 };
 
-javascriptGenerator['sensing_health'] = (block) => {
+javascriptGenerator['sensing_health'] = function (block) {
   let code = '';
-  if (javascriptGenerator.STATEMENT_PREFIX) {
-    code += javascriptGenerator.injectId(javascriptGenerator.STATEMENT_PREFIX, block);
+  if (this.STATEMENT_PREFIX) {
+    code += this.injectId(this.STATEMENT_PREFIX, block);
   }
   code += `target.util.health`;
-  return [code, javascriptGenerator.ORDER_FUNCTION_CALL];
+  return [code, this.ORDER_FUNCTION_CALL];
 };
